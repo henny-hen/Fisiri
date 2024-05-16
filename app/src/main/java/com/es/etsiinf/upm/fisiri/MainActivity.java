7fc9c8b72214792248e0d181c7fb651fa043ef30
Henny Sanchez <henny.sanchez@dzsi.com>
2024-05-17 01:24:13 +0200

    Creación de GUI.

--- app/src/main/java/com/es/etsiinf/upm/fisiri/MainActivity.java
+++ app/src/main/java/com/es/etsiinf/upm/fisiri/MainActivity.java
@@ -2,7 +2,13 @@ package com.es.etsiinf.upm.fisiri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView fisiri= findViewById(R.id.FISIRI_big);
+        Animation fadeinan = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        fisiri.startAnimation(fadeinan);
+
+        fadeinan.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                fisiri.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        final Button botonstart = findViewById(R.id.button);
+        botonstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cambio = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(cambio);
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+            }
+        });
+
+
     }
 }
