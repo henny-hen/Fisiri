456acc518b9ce5b89b5b9eae3ab86a6f3851af3a
Henny Sanchez <henny.sanchez@dzsi.com>
2024-05-17 02:55:24 +0200

    2 pantalla GUI.

--- app/src/main/java/com/es/etsiinf/upm/fisiri/MainActivity2.java
+++ app/src/main/java/com/es/etsiinf/upm/fisiri/MainActivity2.java
@@ -3,6 +3,10 @@ package com.es.etsiinf.upm.fisiri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        final TextView text1= findViewById(R.id.title_record);
+        final TextView text2 = findViewById(R.id.title_act);
+
+
+        Animation fadeinan = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        text1.startAnimation(fadeinan);
+        text2.startAnimation(fadeinan);
+
+        fadeinan.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                text1.setVisibility(View.VISIBLE);
+                text2.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
     }
 }
